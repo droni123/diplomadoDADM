2502dd5bfe4f90851102b24c21375e8147c88f90
Idel <idlch.perte@gmail.com>
2022-10-28 19:51:17 -0500

    Estados de App

--- app/src/main/java/mx/com/idel/my/diplomado/MainActivity.kt
+++ app/src/main/java/mx/com/idel/my/diplomado/MainActivity.kt
@@ -2,10 +2,39 @@ package mx.com.idel.my.diplomado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    private var TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
+        //crea | construye | arranca app
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    override fun onStart() {
+        //inicia app
+        super.onStart()
+        Log.e(TAG, "${getString(R.string.entre_en)} onStart")
+    }
+    override fun onResume() {
+        //activa app
+        super.onResume()
+        Log.e(TAG,"${getString(R.string.entre_en)} onResume")
+    }
+    override fun onPause() {
+        //pausa app
+        super.onPause()
+        Log.e(TAG,"${getString(R.string.entre_en)} onPause")
+    }
+    override fun onRestart() {
+        //restaura app
+        super.onRestart()
+        Log.e(TAG,"${getString(R.string.entre_en)} onRestart")
+    }
+    override fun onDestroy() {
+        //destruye app
+        super.onDestroy()
+        Log.e(TAG,"${getString(R.string.entre_en)} onDestroy")
+    }
+
 }
