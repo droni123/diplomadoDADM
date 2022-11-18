643a72d21368304258fad5f97cbec0b87ae51239
Idel <idlch.perte@gmail.com>
2022-11-18 17:25:37 -0600

    Clase 7 - View Brinding

--- app/src/main/java/mx/com/idel/diplomado/MainActivity.kt
+++ app/src/main/java/mx/com/idel/diplomado/MainActivity.kt
@@ -2,10 +2,18 @@ package mx.com.idel.diplomado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.com.idel.diplomado.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var brinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        brinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(brinding.root)
+
+        brinding.textoUno.text = getString(R.string.texto1)
+        brinding.textoDos.text = getString(R.string.texto2)
+        brinding.textoTres.text = getString(R.string.texto3)
+
     }
 }
